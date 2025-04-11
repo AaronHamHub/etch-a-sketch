1b12538b7605c2fe5b28f3a725b9b764eef8d072
Aaron Hammer-Huber <202416678+AaronHamHub@users.noreply.github.com>
2025-04-11 11:45:53 -0400

    Basic functionality with working grid, and commented code

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
+// Sets up the grid to 
 function setupGrid(sideLength)
 {
+    // Fetch the grid container
     const container = document.querySelector("#container");
 
+    // Fill the grid with divs
     for(let r = 0; r < sideLength; r++)
     {
+        // Make a row to go in the grid
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
         for(let c = 0; c < sideLength; c++)
         {
+            // Make a cell to go in the row
             const cell = document.createElement("div");
             cell.classList.add("cell");
             row.appendChild(cell);
-            //cell hover listener event
+            // Add a listener to change the background when the mouse hovers over it
+            cell.addEventListener("mouseenter", function (e)  {
+                e.target.style.background = "black";
+            });
         }
     }
 }
 
 
 
+// Start with a default 16 x 16 grid
 setupGrid(16);
